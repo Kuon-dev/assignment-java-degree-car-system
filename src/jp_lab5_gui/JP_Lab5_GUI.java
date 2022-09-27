7f10ba05997be7513498fd09ff56d491d546111e
katherine0241 <83446472+katherine0241@users.noreply.github.com>
2022-09-27 17:48:07 +0800

    test

---
+++ src/jp_lab5_gui/JP_Lab5_GUI.java
@@ -0,0 +1,11 @@
+//WAP to accept name from user and display it using GUI
+package jp_lab5_gui;
+
+public class JP_Lab5_GUI {
+
+    public static void main(String[] args) {
+        TestingGUI tg = new TestingGUI();
+        tg.setVisible(true);
+    }
+    
+}
